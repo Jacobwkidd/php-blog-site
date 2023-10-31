6af4937c4b9b0c6cc51a625f3b9219a6aa76e1bc
Jacob Kidd <kiddj1@students.westerntc.edu>
2023-10-31 14:44:33 -0500

    to be continued..

--- blog/index.php
+++ blog/index.php
@@ -4,12 +4,51 @@ $pageTitle = "Blog";
 $pageDescription = "A listing of links to blog posts";
 include("../includes/header.inc.php");
 // echo($x);
+
+$host = "localhost";
+$db = "php_blog_site";
+$user = "root";
+$password = "test";
+
+$link = mysqli_connect($host, $user, $password, $db);
+// var_dump($link);
+
+$qStr = "SELECT * FROM pages WHERE published IS NOT NULL";
+
+$result = mysqli_query($link, $qStr);
+
+// var_dump($result);
+$allPosts = [];
+while($row = mysqli_fetch_assoc($result)){
+	$post = [];
+	$post["id"] = htmlentities($row['id']);
+	$post["title"] = htmlentities($row['title']);
+	$post["description"] =htmlentities($row['description']);
+	$allPosts[] = $post;
+}
+//var_dump($allPosts);
+
+function createPostList($posts){
+	$html = "<ol>";
+	foreach($posts as $p){
+		$html .= "<li>";
+		$html .= "<h2><a href='blog-page.php?id={$p["id"]}'>{$p["title"]}</a></h2>";
+		$html .= "<p>{$p["description"]}</p>";
+		$html .= "</li>";
+	}
+
+	$html .= "</ol>";
+	return $html;
+}
+
+
+
 ?>
 
 
 		<main>
 			<h1>Blog List</h1>
-			<p>Links go here...</p>
+			<?php echo(createPostList($allPosts));?>
 		</main>
 		<aside>
 				Side Bar
